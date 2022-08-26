67a6861103be492c2012de0c269a5e8e94011194
justin creaby <justincreaby@gmail.com>
2022-08-26 16:36:55 -0600

    Clean up rviz files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/parallels/catkin_go_chase_it_ws/build/gtest/cmake_install.cmake")
   include("/home/parallels/catkin_go_chase_it_ws/build/my_robot/cmake_install.cmake")
+  include("/home/parallels/catkin_go_chase_it_ws/build/teleop_twist_keyboard/cmake_install.cmake")
   include("/home/parallels/catkin_go_chase_it_ws/build/ball_chaser/cmake_install.cmake")
 
 endif()
---
+++ build/teleop_twist_keyboard/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/catkin_go_chase_it_ws/src/teleop_twist_keyboard
+# Build directory: /home/parallels/catkin_go_chase_it_ws/build/teleop_twist_keyboard
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
